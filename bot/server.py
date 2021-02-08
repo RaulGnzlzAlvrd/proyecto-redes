e79eb3e0f183f348372171afb6e9a6efa89de9db
Raúl González <gonzalv@ciencias.unam.mx>
2021-02-08 14:06:47 -0600

    Arregla el formato y datos

--- bot/server.py
+++ bot/server.py
@@ -7,14 +7,15 @@ app = Flask(__name__)
 api = Api(app)
 
 class ServerInfo(Resource):
-        def get(self): 
-            cpu = psutil.cpu_freq()[0]  ## Tupla nombrada con estadisticas del CPU.
-            memory = psutil.virtual_memory()[0] ## Tupla nombrada con estadisticas de la memoria.
-            netmbps = speedtest.Speedtest().download() ##Velocidad de descarga del servidor.
-            return {"cpu": cpu, "memory": memory, "net-mbps": netmbps}
+        def get(self):
+            cpu = psutil.getloadavg()[0] * 100  ## Tupla nombrada con estadisticas del CPU.
+            memory = psutil.virtual_memory()[5] / 1024 / 1024 ## Tupla nombrada con estadisticas de la memoria.
+            netmbps = speedtest.Speedtest().download() / 1024 / 1024 ##Velocidad de descarga del servidor.
+            return {"cpu": cpu, "memory": memory, "net-mbps": netmbps, "status": "SUCCESS"}
 
 api.add_resource(ServerInfo, "/serverinfo")
 
 
-if __name__ == "__main__":  
+if __name__ == "__main__":
     app.run(debug=True)
+
