7fe9f06bf312819da9c104653461e8f7772b9700
Miren J. Hernández <30506332+mjhl1999@users.noreply.github.com>
2021-02-12 19:25:27 -0600

    documentación actualizada

--- bot/server.py
+++ bot/server.py
@@ -1,11 +1,15 @@
+# Código para crear una instancia de Flask.
+
 from flask import Flask
 from flask_restful import Api, Resource
 import psutil
 import speedtest
 
+# param __name__ sirve para indicar la ubicación de la aplicación.
 app = Flask(__name__)
 api = Api(app)
 
+# Clase que obtiene los datos  del servidor de manera "oculta" al usuario.
 class ServerInfo(Resource):
         def get(self):
             cpu = psutil.getloadavg()[0] * 100  ## Tupla nombrada con estadisticas del CPU.
